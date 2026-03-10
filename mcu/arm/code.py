c9ce9a375d0bc3aab0f3d8a5378ec8a6c095b991
Eric Huang <xilahuade@foxmail.com>
2026-03-10 19:00:04 -0400

    Pico W can now locally communicate with local server

--- mcu/arm/code.py
+++ mcu/arm/code.py
@@ -8,20 +8,21 @@ import adafruit_requests as requests
 
 
 SSID, PASSWORD = os.getenv("WIFI_SSID"), os.getenv("WIFI_PASSWORD")
-BASE_URL = "http://127.0.0.1:5000"
+BASE_URL = "http://172.20.10.3:8000"
 
 
 def main() -> None:
     wifi.radio.connect(SSID, PASSWORD)
+    print("Connected:", wifi.radio.ipv4_address)
 
     pool = socketpool.SocketPool(wifi.radio)
     http = requests.Session(pool)
 
     data = {"testing": 0}
 
-    response = http.post(f"{BASE_URL}/receive", data=json.dumps(data))
+    response = http.post(f"{BASE_URL}/receive", json=data)
 
-    print(response)
+    print(response.json())
 
 
 if __name__ == "__main__":
